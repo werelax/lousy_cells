40992659755a9154e7792167e05ebfbfe4cd568c
Elías Alonso <eliasagc@gmail.com>
2011-10-24 01:16:53 +0200

    Polishing a little big LousyTemp

--- lousy_template.js
+++ lousy_template.js
@@ -1,9 +1,8 @@
 function LousyTemp(text) {
   var code = '%>' + text + '<%';
   code = code
-    .replace(/[\n\r\t]/g," ")
-    .replace(/"/g, '\\"')
-    .replace(/'/g, "\\'")
+    .replace(/[\n\r\t]/g,' ')
+    .replace(/(["'])/g, '\\$1')
     .replace(/<%=(.*?)%>/g, "', $1, '")
     .replace(/%>(.*?)<%/g, "_t_.push('$1'); ");
   code = "var _t_ = []; with(obj) {" + code + "}; return _t_.join('');";
